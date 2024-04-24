43c7774207c27f6e806f357fa6417c76ee46470e
Parikhit <parikhitritzz@gmail.com>
2024-04-24 16:54:52 +0530

    Removing CORS

--- server/index.js
+++ server/index.js
@@ -14,11 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-    })
-);
+// app.use(cors());
 
 connectDB();
 
