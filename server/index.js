00a5147f3fac58444d43563bc9617cf052ddddc3
Parikhit <parikhitritzz@gmail.com>
2024-04-24 19:36:01 +0530

    issues fixed

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
