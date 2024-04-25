11ea84de75a29f15e9d8aaffa4463a05f2c8cd72
Parikhit <parikhitritzz@gmail.com>
2024-04-25 10:47:25 +0530

    Resolving issues

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.all(
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.js'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
