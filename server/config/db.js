7ee635e1a8a5f305b1ea7686941e8597920df934
Parikhit <parikhitritzz@gmail.com>
2024-04-25 13:27:12 +0530

    mongo resolved

--- server/config/db.js
+++ server/config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        const connection = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${connection.connection.host}`.cyan.underline.bold);
-    } catch (error) {
-        console.log(err);
-    }
+const connectDB = () => {
+    mongoose
+        .connect(process.env.MONGO_URI)
+        .then(() =>
+            console.log(`MongoDB Connected: ${connection.connection.host}`.cyan.underline.bold)
+        )
+        .catch((error) => console.log(error));
 };
 
 module.exports = connectDB;
