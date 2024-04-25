b8f2ecd95bb7f6a166299b0f8d33f1ed4b54a5ab
Parikhit <parikhitritzz@gmail.com>
2024-04-25 10:37:40 +0530

    Resolving issues

--- server/index.js
+++ server/index.js
@@ -25,10 +25,10 @@ app.all(
     })
 );
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/src/dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist/index.html'));
+    res.sendFile(path.join(__dirname, 'client/src/dist/index.html'));
 });
 
 app.listen(PORT, () => {
