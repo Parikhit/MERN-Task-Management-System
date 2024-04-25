73f948f04e53b8de32cee1e32414a4c852322f4f
Parikhit <parikhitritzz@gmail.com>
2024-04-25 10:52:54 +0530

    Resolving issues

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.all(
     })
 );
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '..', '/client/dist')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
